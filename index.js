0c498be2f14bcab0810398811b4bc3bbab0bd794
Nothings0 <nhanphamx3@gmail.com>
2023-06-23 16:33:55 +0700

    s

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const apiSecure = require("./middleware/apiSecure");
 const app = express();
 app.use(cookies());
 // app.use(cors({ origin: "https://fluxquiz.netlify.app", credentials: true }));
-app.use(cors()); //
+app.use(cors({ origin: "*" })); //
 app.use(express.json({}));
 app.use(morgan("common"));
 // app.use(bodyParser.json({ limit: "50mb" }));
