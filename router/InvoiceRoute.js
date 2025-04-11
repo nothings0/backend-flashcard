9ff27cc2192a60f22ccede8094d30413fb046aff
nothings0 <nhanphamx3@gmail.com>
2025-04-11 15:17:18 +0700

    update 7

--- router/InvoiceRoute.js
+++ router/InvoiceRoute.js
@@ -3,6 +3,6 @@ const { verifyAdmin, verifyToken } = require("../middleware");
 const router = require("express").Router();
 
 router.route("/").post(verifyToken, InvoiceController.createInvoice)
-router.route("/:id").post(verifyToken, InvoiceController.getInvoice)
+router.route("/:id").get(verifyToken, InvoiceController.getInvoice)
 router.route("/webhook-sepay").post(InvoiceController.webhooksepay);
 module.exports = router;
